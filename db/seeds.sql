11f58e60ea7205974eff958c50da9cb4a449c894
Isaac <isaac.jj2016@gmail.com>
2019-09-25 17:12:57 -0500

    initial commit, npm init, npm install, schema and seeds taken care of

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cali Burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Baco Chee", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Juicy Lucy", true);
